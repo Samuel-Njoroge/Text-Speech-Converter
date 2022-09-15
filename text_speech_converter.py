33707f4ff5f0dd915ade0022aefa89640fc2c7d9
Samuel Njoroge <108589210+Samuel-Njoroge@users.noreply.github.com>
2022-09-15 20:42:40 +0300

    Update text_speech_converter.py

--- text_speech_converter.py
+++ text_speech_converter.py
@@ -22,8 +22,26 @@ entry_field.place(x=20,y=100)
 
 #Funtion to convert Text to Speech
 
-def Text_to_Speech:
+def Text_to_Speech():
   Message = entry_field.get()
   speech = gTTS(text = Message)
   speech.save('Converted.mp3')
-  playsound('Conerted.mp3')
+  playsound('Converted.mp3')
+  
+#Exit function
+def Exit():
+  root.destroy()
+  
+ #Reset function
+def Reset():
+  Msg.set("")
+  
+ #Defining buttons
+Button(root, text = "PLAY", font = 'arial 15 bold' , command = Text_to_speech ,width = '4').place(x=25,y=140)
+
+Button(root, font = 'arial 15 bold',text = 'EXIT', width = '4' , command = Exit, bg = 'OrangeRed1').place(x=100 , y = 140)
+
+Button(root, font = 'arial 15 bold',text = 'RESET', width = '6' , command = Reset).place(x=175 , y = 140)
+
+#Executing and running the program
+root.mainloop()
